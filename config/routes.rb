2bd0288a93fd851c8d4f7544c0c5fee6f1751b45
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-05-19 15:01:17 +0900

    reviews are working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
+    member do
+      get :reviews
+    end
   end
 end
